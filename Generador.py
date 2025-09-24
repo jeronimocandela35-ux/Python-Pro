b4110a96740ef9ef67c8cb7526f453c79a3ad430
jeronimocandela35-ux <jeronimocandela35@gmail.com>
2025-09-24 18:30:48 -0400

    Create Generador.py
    My primer generador de contraseñas

---
+++ Generador.py
@@ -0,0 +1,10 @@
+import random
+
+generador = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud = int(input("Introduce la longitud de la contraseña:"))
+contraseña = ""
+
+for i in range (longitud):
+    contraseña += random.choic6
+    e(generador)
+print("Tu contraseña generada es:", contraseña)
